bc77960304b7e37439551c881a43045fbe1d39af
Md Azaz <mdazaz9026@gmail.com>
2023-07-10 20:15:52 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const {home , createUser} = require("../controllers/userControllers.js");
+
+
+
+const router = express.Router();
+
+router.get('/', home)
+
+router.post('/createuser' , createUser)
+
+module.exports = router
